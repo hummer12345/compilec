ab9cf938ce712b2721be924dcf4f219211421622
hummer12345 <69205256+hummer12345@users.noreply.github.com>
2024-02-12 10:32:54 +0000

    aaa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MOD = 998244353;
+const long long INF = 1LL<<60;
 #define rep(i,n) for (int i = 0; i < (int)(n); i++)
 #define int long long
 
