6c87c3f5036faea22f8e9369e0f0b00b8edd168e
hummer12345 <69205256+hummer12345@users.noreply.github.com>
2024-02-13 15:31:15 +0900

    Update setup-aliases.sh

--- setup-aliases.sh
+++ setup-aliases.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "alias ojtest='oj t -c \"./a.out\" -d ./tests/'" >> ~/.bashrc
+
+# コンパイル→テスト
+echo "alias ojtest='g++ -fdiagnostics-color=always -g main.cpp -o a.out && oj t -c \"./a.out\" -d ./tests/'" >> ~/.bashrc
+
+# 提出
 echo "alias submit='acc submit main.cpp'" >> ~/.bashrc
