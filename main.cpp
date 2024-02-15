e30849c4e5040eb2a5b160abdba04227a13aec91
hummer12345 <69205256+hummer12345@users.noreply.github.com>
2024-02-15 23:17:47 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@ const int MOD = 998244353;
 const long long INF = 1LL<<60;
 #define rep(i,n) for (int i = 0; i < (int)(n); i++)
 #define int long long
+#define all(a) (a).begin(), (a).end()
+#define YES cout << "yes" << endl;
+#define NO cout << "no" << endl;
+#define CO(x) cout<<(x);
+#define COEL(x) cout<<(x)<<endl;
+#define PB(a) push_back(a);
+template<typename T>
+using Matrix = vector<vector<T>>;
+template<typename T>
+Matrix<T> make_matrix(int rows, int cols, T value = T()) {
+    return Matrix<T>(rows, std::vector<T>(cols, value));
+}
+// 5行3列の整数型の多次元vectorを生成し、全ての要素を0で初期化
+//auto matrix = make_matrix<int>(5, 3, 0);
 
 signed main() {
-
+    
 }
-
