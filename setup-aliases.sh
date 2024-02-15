02480e610592542fa10ccf529f1abb5a0a1c5e47
hummer12345 <69205256+hummer12345@users.noreply.github.com>
2024-02-15 10:35:31 +0900

    Update setup-aliases.sh

--- setup-aliases.sh
+++ setup-aliases.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # コンパイル→テスト
-echo "alias ts='g++ -fdiagnostics-color=always -g main.cpp -o a.out && oj t -c \"./a.out\" -d ./tests/'" >> ~/.bashrc
+echo "alias ts='g++ -fdiagnostics-color=always -g main.cpp -o a.out && oj t -c \"./a.out\" -d ./tests/ -S'" >> ~/.bashrc
 
 # 提出
-echo "alias submit='acc submit main.cpp'" >> ~/.bashrc
+echo "alias su='acc submit main.cpp'" >> ~/.bashrc
 
 # rmabcエイリアスの追加（ダブルクォーテーションをエスケープ）
 echo "alias rmabc='find . -type d -name \"abc*\" -exec rm -rf {} +'" >> ~/.bashrc
